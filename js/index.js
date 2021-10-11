d528ce64781f0ccd38472f0c1af00f9313ebc0b6
WahyuAgungBudiyanto <s11910102@student.unklab.ac.id>
2021-10-11 16:23:14 +0800

    mid exam

---
+++ js/index.js
@@ -0,0 +1,95 @@
+/* array save faculty + programStudy */
+const faculties = {
+  1: ["Magister Manajemen", "Magister Teologi"],
+  2: ["Ilmu Filsafat"],
+  3: ["Pendidikan Agama", "Pendidikan Bahasa Inggris", "Pendidikan Ekonomi", "Pendidikan Luar Sekolah"],
+  4: ["Akuntansi", "Manejemen"],
+  5: ["Agroteknologi"],
+  6: ["Informatika", "Sistem Informasi"],
+  7: ["Profesi Ners", "Keperawatan"],
+  8: ["Sekretari D3"],
+};
+
+//creating variables to save ID elements
+let facultySelection = document.querySelector("#faculty");
+let prodiSelection = document.querySelector("#pStudy");
+
+//Select dependencies
+facultySelection.addEventListener("change", (e) => {
+  const val = e.target.value;
+
+  let prodies = faculties[val];
+
+  if (!prodies) {
+    pStudy.innerHTML = `<option value="0">-- SELECT PROGRAM STUDY --</option>`;
+    return;
+  }
+  pStudy.innerHTML = prodies.map((p) => `<option value="${p}">${p}</option>`).join("");
+});
+
+//creating variables to save ID element
+const showHide = document.querySelector("#show-hide-btn");
+const form = document.querySelector("#addStudent");
+const table = document.querySelector("table");
+
+/* Show / Hide Form with Button */
+showHide.addEventListener("click", toggleShowHide);
+
+//When mouse click the button, it will run this function
+function toggleShowHide(e) {
+  //if display of the form style is none / hide, it will show the form and change the button text
+  if (form.style.display == "none") {
+    form.style.display = "block";
+    showHide.textContent = "Hide Forms Add New Student";
+    //if display of the form style other than none, it will hide the form and change the button text
+  } else {
+    form.style.display = "none";
+    showHide.textContent = "Show Forms Add New Student";
+  }
+}
+
+/* Add Student */
+form.addEventListener("submit", addStudent);
+
+function addStudent(e) {
+  //get data input
+  let nim = document.querySelector("#studentId").value;
+  let fullName = document.querySelector("#studentName").value;
+  let gender = document.querySelector("input[name=gender]:checked").value;
+  let faculty = document.querySelector("#faculty").options[document.querySelector("#faculty").selectedIndex].textContent;
+  let prodi = document.querySelector("#pStudy").options[document.querySelector("#pStudy").selectedIndex].textContent;
+
+  let template = `
+                  <tr>
+                    <td class="py-3">${nim}</td>
+                    <td class="py-3">${fullName}</td>
+                    <td class="py-3">${gender}</td>
+                    <td class="py-3">${faculty}</td>
+                    <td class="py-3">${prodi}</td>
+                    <td><button class="btn btn-danger btn-md ms-1 rounded-2" onclick="removeRow(this)" type="button" data-toggle="tooltip" data-placement="top" title="Delete"><i class="fa fa-trash"></i></button></td>
+                  </tr>`;
+
+  table.querySelector("tbody").innerHTML += template;
+}
+
+/* Delete row */
+function removeRow(event) {
+  let p = event.parentNode.parentNode;
+  p.parentNode.removeChild(p);
+}
+
+/* Search data by fullName */
+let searchBox = document.querySelector("#search");
+let rows = document.querySelectorAll("tbody tr");
+
+searchBox.addEventListener("keyup", function (e) {
+  const fetch = e.target.value.toLowerCase();
+  rows.forEach((row) => {
+    row.querySelector("td:nth-child(2)").textContent.toLowerCase().includes(fetch) ? (row.style.display = "") : (row.style.display = "none");
+  });
+});
+
+// filter by faculty
+
+
+// filter by program study
